379d4e43ea9db4d0aacbe73616d5de9b34eb2cf6
ViniciusSantoro <vinicius.rio@ifsp.edu.br>
2022-10-05 17:01:25 -0300

    feat: :rocket: add makefile and header

--- Makefile
+++ Makefile
@@ -10,3 +10,37 @@
 #                                                                              #
 # **************************************************************************** #
 
+NAME			=	philo
+HEADER			=	philo.h
+
+INCLUDE        	=    -I ./includes
+
+CC            	=    gcc -Wall -Wextra -Werror -g -pthread
+RM            	=    rm -f
+
+SRC_PATH    	=    ./src/
+OBJ_PATH    	=    ./obj/
+
+SRC_FILES    	=	main.c 
+
+SRC            	=    $(addprefix $(SRC_PATH),$(SRC_FILES))
+OBJ            	=    $(addprefix $(OBJ_PATH),$(subst .c,.o,$(SRC_FILES)))
+
+$(OBJ_PATH)%.o:	$(SRC_PATH)%.c
+				mkdir -p $(OBJ_PATH)
+				$(CC) $(INCLUDE) -c $< -o $@
+
+all:            $(NAME)
+
+$(NAME):        $(LIBFT) $(PRINTF) $(OBJ)
+				$(CC) -o $(NAME) $(OBJ)
+		
+clean:
+				$(RM) $(OBJ)
+
+fclean:            clean
+				$(RM) $(NAME) rm -rf $(OBJ_PATH)
+
+re:				fclean all
+
+.PHONY:			all, clean, fclean, re
--- includes/philo.h
+++ includes/philo.h
@@ -1,20 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vnazioze <vnazioze@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/05 16:28:41 by vnazioze          #+#    #+#             */
-/*   Updated: 2022/10/05 16:28:41 by vnazioze         ###   ########.fr       */
+/*   Created: 2022/10/05 16:58:06 by vnazioze          #+#    #+#             */
+/*   Updated: 2022/10/05 16:58:06 by vnazioze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/philo.h"
+#ifndef PHILO_H
+# define PHILO_H
 
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <stdio.h>
+# include <sys/time.h>
 
-int main()
-{
-    printf("Feynman.");
-    return (0);
-}
+#endif
